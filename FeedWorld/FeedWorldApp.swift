15bc03480ed8a41ef5c2a03381840674d9b06932
Tanya Samastroyenka <samostroenko@applace.io>
2024-02-12 00:34:21 +0300

    Initial Commit

---
+++ FeedWorld/FeedWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  FeedWorldApp.swift
+//  FeedWorld
+//
+//  Created by Tanya Samastroyenka on 12.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct FeedWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
